77186c91010e5a3e97db038f9f050f14cc9033c7
Niceiyke <oyomworld@gmail.com>
2023-05-18 21:48:03 +0100

    dd

--- priceFinder/scraper/tasks.py
+++ priceFinder/scraper/tasks.py
@@ -42,12 +42,13 @@ def scrape():
     @defer.inlineCallbacks
     def crawl():
         yield runner.crawl(jumiaLaptopSpyder)    
-        reactor.stop()
+        
         
     crawl()
     reactor.run() # the script will block here until the last crawl call is finished
 
 scrape()
+reactor.stop()
 
 
     
