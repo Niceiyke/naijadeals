73578727434b19809473c1ea0136a7ba030412fc
Niceiyke <oyomworld@gmail.com>
2023-05-21 11:53:49 +0100

    update

--- priceFinder/priceFinder/settings_file/local.py
+++ priceFinder/priceFinder/settings_file/local.py
@@ -7,9 +7,9 @@ ALLOWED_HOSTS = []
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -37,12 +37,8 @@ CELERY_BEAT_SCHEDULE = {
 """
 
 
-
-
-
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = "static/"
+STATIC_ROOT = "./staticfiles/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
--- priceFinder/products/models.py
+++ priceFinder/products/models.py
@@ -2,18 +2,20 @@ from django.db import models
 
 # Create your models here.
 
+
 class Product(models.Model):
-    name =models.CharField(max_length=1000,primary_key=True)
-    discount_price =models.PositiveIntegerField()
-    original_price =models.PositiveIntegerField()
-    image=models.URLField()
-    product_url=models.URLField()
-    discount_percent=models.PositiveIntegerField()
-    category=models.CharField(max_length=100)
-    stock=models.CharField(max_length=50)
+    name = models.CharField(max_length=1000, primary_key=True)
+    discount_price = models.PositiveIntegerField()
+    original_price = models.PositiveIntegerField()
+    image = models.URLField()
+    product_url = models.URLField()
+    discount_percent = models.PositiveIntegerField()
+    category = models.CharField(max_length=100)
+    stock = models.CharField(max_length=100)
+    store = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
-    class Meta:
-        ordering =['-discount_percent']
 
+    class Meta:
+        ordering = ["-discount_percent"]
